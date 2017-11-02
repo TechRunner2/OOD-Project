c5e67d1c911e22ee8b777a73ac093d8aaf8036ef
TechRunner2 <techrunner2.0@gmail.com>
2017-11-02 16:53:33 -0500

    Organized the get and set methods

--- provider.h
+++ provider.h
@@ -6,17 +6,23 @@ private:
   string company;
   string product;
   string address;
+  double money{0};
 public:
   Provider(string name, string items, string addr) {
     company = name;
     product = items;
     address = addr;
   }
+  //Sets
   void setCompany(string name) { company = name; }
-  string getCompany() { return company; }
   void setProducts(string products_n) { product = products_n; }
-  string getProducts() { return product; }
-
-  string getAddress() { return address; }
   void setAddress(string add) { address = add; }
+  void incMoney(double add) { money += add; }
+  void decMoney(double sub) { money -= sub; }
+
+  //Gets
+  string getCompany() { return company; }
+  string getProducts() { return product; }
+  string getAddress() { return address; }
+  double getMoney() { return money;}
 };
