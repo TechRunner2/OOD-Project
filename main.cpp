57c63bfc42067f9017e5370aabc64ae66606314c
TechRunner2 <techrunner2.0@gmail.com>
2017-11-02 16:50:28 -0500

    Deleted a lot of unnessesary code and moved it elsewhere

--- main.cpp
+++ main.cpp
@@ -3,19 +3,10 @@ Authors: Brandon Behar, Wade Kelly-Higgs, Joseph
 Class: CISC 3321 01
 University: UMHB
 */
-#include <iostream>
-#include <string>
 #include "login.h"
-#include "shell.h"
 using namespace std; // Setting namespace to std so that std:: is not needed
 int main() {
   login ln;
-  shell sh;
-  bool loged_in = ln.begin();
-  if (loged_in == true) {
-    sh.logedin();
-  } else {
-    ln.begin();
-  }
+  ln.begin();
   return 0; // Ends Program
 }
